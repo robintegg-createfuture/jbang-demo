7f550464aaecc900aa09223edc06f6bca7db447a
robintegg-createfuture <robin.tegg@createfuture.com>
2025-06-24 16:36:09 +0100

    add jbang script to run it

---
+++ jbang-nobuild/jbangnobuild.java
@@ -0,0 +1,14 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS https://github.com/robintegg-createfuture/jbang-demo
+
+import com.example.jbang.JBangNoBuildUtils;
+
+public class jbangnobuild {
+
+	public static void main(String... args) {
+
+		JBangNoBuildUtils.printout(args);
+
+	}
+
+}
